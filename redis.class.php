6f0feaab2ef07baebae64e2cc948e4cdc17c51a7
root <root@localhost.localdomain>
2020-03-21 03:41:48 +0000

    fix socket time

--- redis.class.php
+++ redis.class.php
@@ -1,38 +1,52 @@
 <?php
 /**
- * 生产环境建议打开此注释
- * 并在单入口文件中定义diskroom变量
- * 只允许单入口文件访问该文件
+ * 1.   生产环境建议打开此注释
+ *      并在单入口文件中定义diskroom变量
+ *      只允许单入口文件访问该文件
+ *2.  Notice default_socket_timeout 配置项需要放到connect之前 否则无法覆盖php.ini中的配置
  */
 if(!defined(DISKROOMS)){
     exit('hacks');
 }
 
 class Hredis{
-    
+    private static $redis_config = [];
     private static $instance_redis = NULL; 
     
     public function __construct($ip,$port = '6379',$pwd = ''){
+        self::$redis_config = [
+            'ip'=>$ip,
+            'port'=>$port,
+            'pwd'=>$pwd
+        ];
         if(self::$instance_redis == null){
             self::$instance_redis = new Redis();
         }
         if(!self::$instance_redis instanceof Redis){
             throw new Exception('实例化未完成');
-         } 
-         //Notice default_socket_timeout 配置项需要放到connect之前 否则无法覆盖php.ini中的配置
-         ini_set('default_socket_timeout', 1);
-        self::$instance_redis->connect($ip,$port);
-        if(!empty($pwd)){
-            self::$instance_redis->auth($pwd);
-          }
-        $this->testNotifyKeyEvent();
+         }
+    }
+    
+    /**
+     * 连接 redis 服务器
+    * @param integer $default_socket_timeout 客户端socket超时时间
+     */
+    private function _conncet($default_socket_timeout =  3){
+        set_time_limit(0);
+        ini_set('default_socket_timeout', $default_socket_timeout);
+        self::$instance_redis->connect(self::$redis_config['ip'],self::$redis_config['port']);
+        if(!empty(self::$redis_config['pwd'])){
+            self::$instance_redis->auth(self::$redis_config['pwd']);
+        }
     }
     
     /**
      * 测试redis服务 notify-keyspace-events 配置是否打开
+     * 设置socket过期时间 2秒后还未收到消息即说明没有配置 notify-keyspace-events 参数
      */
     public function testNotifyKeyEvent(){
-        ini_set('default_socket_timeout', 3);  //设置socket过期时间 2秒后还未收到消息即说明没有配置 notify-keyspace-events 参数
+        $this->_conncet();
+            //设置一秒过期
         self::$instance_redis->setex("testNotifyKeyEvent",1,"testNotifyKeyEvent");
            try{
         self::$instance_redis->subscribe(array('__keyevent@0__:expired'),function($instance,$channelName, $message){
@@ -53,8 +67,7 @@ class Hredis{
      * 注册监听频道函数
      */
     public function registerListenFunction($callbackFunc = '',$channelName = '__keyevent@0__:expired'){
-        set_time_limit(0);
-        ini_set('default_socket_timeout', 3);  //保持连接
+        $this->_conncet(-1);
         self::$instance_redis->subscribe(array($channelName),$callbackFunc);
     }
     
